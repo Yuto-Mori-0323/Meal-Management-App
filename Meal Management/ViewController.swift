61feeee005549fca868049a105c1a2180f23c2fd
Yuto Mori <ymori_0323@yahoo.co.jp>
2018-12-23 23:36:33 +0900

    Initial Commit

---
+++ Meal Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meal Management
+//
+//  Created by 森勇人 on 2018/12/23.
+//  Copyright © 2018年 yuto mori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
