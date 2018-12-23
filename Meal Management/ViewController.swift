bd45d8a7e39639049c55252c35a9d48ab3c248c0
Yuto Mori <ymori_0323@yahoo.co.jp>
2018-12-24 01:25:46 +0900

    ページ遷移作成

--- Meal Management/ViewController.swift
+++ Meal Management/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func gotoRegistration(_ sender: Any) {
+        // 移動先のビューコントローラを参照する
+        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "Search Page")
+        // トランジションの映像効果を指定する
+        nextVC?.modalTransitionStyle = .coverVertical
+        // シーンを移動する
+        present(nextVC!, animated: true, completion: nil)
+    }
+    
+    @IBAction func gotoSearch(_ sender: Any) {
+        // 移動先のビューコントローラを参照する
+        let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "Registration Page")
+        // トランジションの映像効果を指定する
+        nextVC?.modalTransitionStyle = .coverVertical
+        // シーンを移動する
+        present(nextVC!, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ Meal Management/registration Page ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  registration Page ViewController.swift
+//  Meal Management
+//
+//  Created by 森勇人 on 2018/12/24.
+//  Copyright © 2018年 yuto mori. All rights reserved.
+//
+
+import UIKit
+
+class registration_Page_ViewController: UIViewController {
+    
+
+    
+    @IBAction func gotoTop(_ sender: Any) {
+        // 現在のシーンを閉じて元のシーンに戻る
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
