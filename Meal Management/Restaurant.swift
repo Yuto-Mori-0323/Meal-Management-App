f733dcdbd0dd3f2d384e32578a0de121992c15a5
Yuto Mori <ymori_0323@yahoo.co.jp>
2018-12-31 01:55:19 +0900

    Realmクラス設定

---
+++ Meal Management/Restaurant.swift
@@ -0,0 +1,19 @@
+//
+//  Restaurant.swift
+//  Meal Management
+//
+//  Created by 森勇人 on 2018/12/31.
+//Copyright © 2018 yuto mori. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Restaurant: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
